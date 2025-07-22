5eb83dd2df7e60a807c0bb3b50de128a6d4a6e8b
Viktor <viktoryanenko11@gmail.com>
2025-07-22 20:44:54 +0300

    add utils, generate one and some contacts scripts

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -2,6 +2,10 @@ import { PATH_DB } from '../constants/contacts.js';
 import fs from 'node:fs/promises';
 
 export const readContacts = async () => {
-  const buffer = await fs.readFile(PATH_DB);
-  console.log(buffer);
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    return JSON.parse(data);
+  } catch (error) {
+    console.error('Reading file error:', error);
+  };
 };
