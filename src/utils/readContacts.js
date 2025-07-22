ca75b9cb2784389839825a4452aca423e5fc2de7
Viktor <viktoryanenko11@gmail.com>
2025-07-22 19:29:19 +0300

    minor add at work

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,7 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  const buffer = await fs.readFile(PATH_DB);
+  console.log(buffer);
+};
